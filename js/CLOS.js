dc6eeb2d589cd26e06d9da3a424ce8d9ad96c923
nuoby <ojamajo321@gmail>
2017-05-01 02:42:04 +0800

    commit1

---
+++ js/CLOS.js
@@ -0,0 +1,246 @@
+﻿
+
+function CHANGE01(A){
+if(A=="A"){
+document.getElementById("V01").style.backgroundColor="#00CCFF";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/020100.htm";
+}else if(A=="B"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#00CCFF";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/030100.htm";
+}else if(A=="C"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#333333";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/010300.htm";
+}else if(A=="D"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#00CCFF";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/040100.htm";
+}else if(A=="E"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#00CCFF";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/050100.htm";
+}else if(A=="F"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#00CCFF";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/060100.htm";
+}else if(A=="G"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#00CCFF";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/070100.htm";
+}else if(A=="H"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#00CCFF";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/080100.htm";
+}else if(A=="I"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#00CCFF";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/090100.htm";
+}else if(A=="J"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#00CCFF";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/100100.htm";
+}else if(A=="K"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#00CCFF";
+document.getElementById("V12").style.backgroundColor="#000000";
+iframe01.location.href="page/110100.htm";
+}else if(A=="L"){
+document.getElementById("V01").style.backgroundColor="#000000";
+document.getElementById("V02").style.backgroundColor="#000000";
+document.getElementById("V03").style.backgroundColor="#000000";
+document.getElementById("V04").style.backgroundColor="#000000";
+document.getElementById("V05").style.backgroundColor="#000000";
+document.getElementById("V06").style.backgroundColor="#000000";
+document.getElementById("V07").style.backgroundColor="#000000";
+document.getElementById("V08").style.backgroundColor="#000000";
+document.getElementById("V09").style.backgroundColor="#000000";
+document.getElementById("V10").style.backgroundColor="#000000";
+document.getElementById("V11").style.backgroundColor="#000000";
+document.getElementById("V12").style.backgroundColor="#00CCFF";
+iframe01.location.href="page/120100.htm";
+}
+}
+function GO0105() {
+if (event.wheelDelta >= 120){
+	document.location.href="010500.htm";
+}else{
+	document.location.href="010512.htm"
+}
+}
+
+
+
+function GO011001() {
+if (event.wheelDelta >= 120){
+	document.location.href="011000.htm";
+}else{
+	document.location.href="011012.htm"
+}
+}
+
+function GO0110() {
+if (event.wheelDelta >= 120){
+	document.location.href="011000.htm";
+}else{
+	document.location.href="011024.htm"
+}
+}
+
+function GO011024() {
+if (event.wheelDelta >= 120){
+	document.location.href="011012.htm";
+}else{
+	document.location.href="011024.htm"
+}
+}
+
+
+function GO011201() {
+if (event.wheelDelta >= 120){
+	document.location.href="011200.htm";
+}else{
+	document.location.href="011212.htm"
+}
+}
+
+function GO0112_1() {
+if (event.wheelDelta >= 120){
+	document.location.href="011200.htm";
+}else{
+	document.location.href="011224.htm"
+}
+}
+function GO0112_2() {
+if (event.wheelDelta >= 120){
+	document.location.href="011212.htm";
+}else{
+	document.location.href="011236.htm"
+}
+}
+
+function GO011236() {
+if (event.wheelDelta >= 120){
+	document.location.href="011224.htm";
+}else{
+	document.location.href="011236.htm"
+}
+}
+
+function GO0113() {
+if (event.wheelDelta >= 120){
+	document.location.href="011300.htm";
+}else{
+	document.location.href="011312.htm"
+}
+}
